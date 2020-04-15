dce5eb05c40810dffa01faf9117ef2ff06d7f8a0
maddybvh <maddybvh@gmail.com>
2020-04-15 09:23:12 -0400

    Fixes typos

--- script.js
+++ script.js
@@ -1,43 +1,61 @@
+const playerFactory = (symbol) => {
+  let squares = [];
+  const addSquare = (sqaureID) => {
+    squares.push(sqaureID);
+  };
+  
+  return {
+    symbol,
+    squares,
+    addSquare,
+  };
+};
+
+const player1 = playerFactory('X')
+const player2 = playerFactory('O')
+var currentPlayer = player1
+
+const winingCombinations = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9],
+  [1,4,7],
+  [2,5,8],
+  [3,6,9],
+  [1,5,9],
+  [3,5,7]
+]
+
+const GameBoard = (() => {
+
+
+
+
+})();
+
 const DisplayContoller =(() =>{
-  const sqaures = document.querySelectorAll('.sqaure');
-  squares.forEach((sqaure) => {square.addEventListener('click', (e) => {
+  const squares = document.querySelectorAll('.square');
+  squares.forEach((square) => {square.addEventListener('click', (e) => {
     //If the square is empty    
     if (!e.target.textContent){
       //Add the current players mark to the square
-      e.target.textContent = currentplayer.symbol
+      e.target.textContent = currentPlayer.symbol
       //Push the square id to the current players' squares
-      currentplayer.addSqaure(square.id)
+      currentPlayer.addSquare(square.id)
       checkWinner()       
       changePlayer()    
     }
     });
   });
-})();
 
-const GameBoard = (() => {
-  const winingCombinations = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7]
-  ]
-
-  const player1 = playerFactory('X')
-  const player2 = playerFactory('O')
-  let currentPlayer = player1
-  
   //--- check if current player has the winning numbers
   function checkWinner (){
     if (currentPlayer.squares.length > 4){
         alert("It's a tie")
     }else if (currentPlayer.squares.length > 2) {
         for (let i = 0; i < winingCombinations.length; i++){
-            if (checker(currentplayer.squares, winingCombinations[i]) === true){
-                alert(currentplayer.symbol + "has won!")
+            if (checker(currentPlayer.squares, winingCombinations[i]) === true){
+                alert(currentPlayer.symbol + "has won!")
                 break
             }}}
   }
@@ -50,15 +68,6 @@ const GameBoard = (() => {
 
 })();
 
-const playerFactory = (symbol) => {
-  let squares = [];
-  const addSquare = (sqaureID) => {
-    squares.push(parseInt(item.dataset.number));
-  };
-  
-  return {
-    symbol,
-    squares,
-    addSquare,
-  };
-};
+
+
+
